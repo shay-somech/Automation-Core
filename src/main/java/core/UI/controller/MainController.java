9bf9db63281663fca8c52dac43fe458e05bc6984
Automation <>
2019-05-26 12:25:22 +0300

    Fixed ActionEvent handler bug

--- src/main/java/core/UI/controller/MainController.java
+++ src/main/java/core/UI/controller/MainController.java
@@ -4,6 +4,7 @@ import core.UI.application.Main;
 import core.UI.controller.tab.Tab1Controller;
 import core.UI.controller.tab.Tab2Controller;
 import core.utils.Log;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -12,7 +13,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
